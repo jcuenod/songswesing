165363aa0e52cb181295b76711e83f4fb3279077
James Cuénod <j3frea@gmail.com>
2014-12-26 13:08:12 +0200

    Removed pointless debugging

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,9 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     super do |resource|
-      logger.debug "fish"
-      logger.debug params[:user].inspect
-      logger.debug "fingers"
       resource.church = Church.find_or_create_by(:church_name => params[:user][:church_attributes][:church_name])
     end
   end
