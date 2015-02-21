edc4bbc0f96700a8762094eebcb4c49c5214f5fd
James Cuénod <j3frea@gmail.com>
2015-02-21 19:36:00 +0200

    Added exhaustive usage table (looks ugly) at /usages Limited to church of current_user

--- app/controllers/usages_controller.rb
+++ app/controllers/usages_controller.rb
@@ -1,6 +1,6 @@
 class UsagesController < ApplicationController
 	def index
-		@usages = Usage.all
+		@usages = Usage.group(:song_id, :song_name).joins(:song, :service).where(:services => {church_id: current_user.church_id}).order("count_all DESC").count
 	end
 	def data
 		@usage_data = Usage.limit(params[:limit]).group(:song_id, :song_name).joins(:song, :service).where(:services => {church_id: current_user.church_id}).order("count_all DESC").count
