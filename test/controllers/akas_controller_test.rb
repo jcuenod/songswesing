560f642e8603472b04b123f3e89e74d52e47e59a
James Cuénod <j3frea@gmail.com>
2015-02-25 23:10:47 +0200

    Added test for data-song-id in aka index (it's used by init.js for insertion)

--- test/controllers/akas_controller_test.rb
+++ test/controllers/akas_controller_test.rb
@@ -4,4 +4,8 @@ class AkasControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "we can find song-ids" do
+  	get :index
+  	assert_select "[data-song-id]"
+  end
 end
