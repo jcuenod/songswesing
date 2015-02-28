29b4060400d69d4abc7213c3ceda502c02dac090
James Cuénod <j3frea@gmail.com>
2015-02-28 17:22:40 +0200

    Hopefully functional most recent usage date is now attached to usage data (I'm not entirely confident that the date is accurate though)

--- app/controllers/usages_controller.rb
+++ app/controllers/usages_controller.rb
@@ -1,9 +1,10 @@
 class UsagesController < ApplicationController
 	def index
-		@usages = Usage.group(:song_id, :song_name).joins(:song, :service).where(:services => {church_id: current_user.church_id}).order("count_all DESC").count
+		@usages = Usage.select("MAX(services.date) as date", :song_name, :song_id, "count(song_id) as count_song_id").joins(:service, :song).group(:song_name, :song_id).order("count_song_id DESC")
 	end
 	def data
-		@usages = Usage.limit(params[:limit]).group(:song_id, :song_name).joins(:song, :service).where(:services => {church_id: current_user.church_id}).order("count_song_id DESC").count :song_id
+		@usages = Usage.limit(params[:limit]).select("MAX(services.date) as date", :song_name, :song_id, "count(song_id) as count_song_id").joins(:service, :song).group(:song_name, :song_id).order("count_song_id DESC")
+		#@usages = Usage.limit(params[:limit]).group(:song_id, :song_name).joins(:song, :service).where(:services => {church_id: current_user.church_id}).order("count_song_id DESC").count :song_id
 		render :partial => "usage_table"
 	end
 end
