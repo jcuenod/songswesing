3da05782d7f53897fe5d68a5c185ac11c8429df2
James Cuénod <j3frea@gmail.com>
2015-03-05 15:43:35 +0200

    Removed annoying styling of focussed link Attempted to fix the featherlight hide through ccli_loader refactoring

--- app/assets/javascripts/ccli_load.js
+++ app/assets/javascripts/ccli_load.js
@@ -4,24 +4,26 @@ function ccli_loader ()
 	var jqxhr = $.ajax({
 		'url': "/songassist",
 		"type": 'POST',
-		"data": $("#song_ccli_number").serialize()
-    })
-	.done(function(data) {
-	  	$(".ccli_populate").removeClass("glyphicon-refresh glyphicon-spin").addClass("glyphicon-cloud-download")
+		"data": $("#song_ccli_number").serialize(),
+		"success": function(data) {
+		  	$(".ccli_populate").removeClass("glyphicon-refresh glyphicon-spin").addClass("glyphicon-cloud-download")
 
-	    $("#song_song_name").val( data.title );
-	    $("#song_writers").val(   data.authors );
-	    $("#song_license").val(   data.copyright );
+		    $("#song_song_name").val(data.title);
+		    $("#song_writers").val(data.authors);
+		    $("#song_license").val(data.copyright);
 
-	    for (st in data.search_terms) {
-	    	$(".add_fields").click()
-	    	$(".nested-fields").last().find("input[id$='display_text']").val( data.search_terms[st] );
-	    }
-	})
-	.fail(function(e) {
-		alert ("Sorry, either the server was too slow or your song can't be found, try looking up this url:\n" + "https://us.songselect.com/songs/" + $("#song_ccli_number").val());
-		$.featherlight.close();
-		console.log("Here's the real error:");
-		console.log(e);
+		    for (st in data.search_terms) {
+		    	$(".add_fields").click()
+		    	$(".nested-fields").last().find("input[id$='display_text']").val(data.search_terms[st]);
+		    }
+		},
+		"error": function(a,b,c) {
+			alert ("Sorry, either the server was too slow or your song can't be found, try looking up this url:\n" + "https://us.songselect.com/songs/" + $("#song_ccli_number").val());
+			$.featherlight.close();
+			console.log("Here's the real error:");
+			console.log(a);
+			console.log(b);
+			console.log(c);
+		}
 	});
 }
