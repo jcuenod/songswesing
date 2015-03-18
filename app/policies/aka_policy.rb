c7471c8cc0846758f5ae869abf6ae4ff847d222a
James Cuénod <j3frea@gmail.com>
2015-03-19 00:30:21 +0200

    Added create? method for aka_policy (otherwise no one can create songs, I should think)

--- app/policies/aka_policy.rb
+++ app/policies/aka_policy.rb
@@ -1,4 +1,8 @@
 class AkaPolicy < ApplicationPolicy
+  def create?
+    #must be same or less restrictive than song.create? policy
+    user.church_admin? || user.admin?
+  end
   def update?
     user.admin?
   end
