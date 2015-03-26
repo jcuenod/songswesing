55beb1a71e8ae49d22d72ca74762915b1ffa3c78
James Cuénod <j3frea@gmail.com>
2015-03-26 21:02:17 +0200

    New User admin page (not actually functional but it's starting to look like something)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	def index
-		if current_user.admin?
-			@users = User.all
+		if current_user.admin? || current_user.church_admin?
+			@users = policy_scope(User.joins :church).order :church_id
 		else
 			redirect_to root_path
 		end
