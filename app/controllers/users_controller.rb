ad5167651433f610b7fb0bc6291b5935d37caf82
James Cuénod <j3frea@gmail.com>
2015-03-27 09:40:03 +0200

    Church Leaders can view but not modify things. Only "Super Users" can delete users (But church admins can disable them)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-		if current_user.admin? || current_user.church_admin?
+		if current_user.admin? || current_user.church_admin? || current_user.church_leader?
 			@users = policy_scope(User.joins :church).order :church_id
 		else
 			redirect_to root_path
