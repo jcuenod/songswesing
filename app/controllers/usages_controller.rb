6c8ef6cd32521c0fc3f84d5b9ae210432e9a7d79
James Cuénod <j3frea@gmail.com>
2015-04-01 17:52:29 +0200

    Filtering by song tags on usage table

--- app/controllers/usages_controller.rb
+++ app/controllers/usages_controller.rb
@@ -1,6 +1,12 @@
 class UsagesController < ApplicationController
 	def index
 		@usages = policy_scope(Usage.joins :service).select("MAX(services.date) as date", :song_name, :song_id, "count(song_id) as count_song_id").joins(:service, :song).group(:song_name, :song_id).order("count_song_id DESC")
+
+    @song_tags = @usages.map do |usage|
+      SongTag.where(church_id: current_user.church_id, song_id: usage.song_id).map do |st|
+        Tag.find(st.tag_id).name
+      end
+    end
 	end
 	def data
 		@usages = policy_scope(Usage.joins :service).limit(params[:limit]).select("MAX(services.date) as date", :song_name, :song_id, "count(song_id) as count_song_id").joins(:service, :song).group(:song_name, :song_id).order("count_song_id DESC")
