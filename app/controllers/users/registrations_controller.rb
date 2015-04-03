4237133780df27018a0f0d3384b0d9383861e132
James Cuénod <j3frea@gmail.com>
2015-04-03 20:25:45 +0200

    Fix for failed sign up (church_attributes not permitted). Closes #34

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters
+
   # POST /resource
   def create
     super do |resource|
       resource.church = Church.find_or_create_by(:church_name => params[:user][:church_attributes][:church_name])
     end
   end
+
+  protected
+    # For custom field (church_attributes)
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:email, :password, :password_confirmation, :church_attributes => [ :church_name ])
+      end
+    end
 end
