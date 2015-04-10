f417f19b90c33b0a88340293d0c312a26bb89cc6
James Cuénod <j3frea@gmail.com>
2015-04-10 19:07:27 +0200

    Fixed sign_up to allow existing church (with find_or_create_by)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
+  skip_before_action :verify_authenticity_token
 
   # POST /resource
   def create
-    super do |resource|
-      resource.church = Church.find_or_create_by(:church_name => params[:user][:church_attributes][:church_name])
-    end
+    params[:user][:church_id] = Church.find_or_create_by(:church_name => params[:user][:church_attributes][:church_name]).id
+
+    super
 
     if @user.persisted?
       UserMailer.send_notify_admin_of_new_users(@user).deliver_later
@@ -16,7 +17,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # For custom field (church_attributes)
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit(:email, :password, :password_confirmation, :church_attributes => [ :church_name ])
+        # :church_id is not going to actually arrive (:church_name will, as part of nested attributes), but we'll chuck it in there in our overridden #create method
+        u.permit(:email, :password, :password_confirmation, :church_id)
       end
     end
 end
