e77a6aa4e227065c99663447fae7ed2c5feae499
James Cuénod <j3frea@gmail.com>
2015-04-11 14:14:06 +0200

    Users page orders by email adress (as well as "enabled" and grouping of churches)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	def index
 		if current_user.admin? || current_user.church_admin? || current_user.church_leader?
-			@users = policy_scope(User.joins :church).order :church_id, "enabled DESC"
+			@users = policy_scope(User.joins :church).order :church_id, "enabled DESC", :email
 		else
 			redirect_to root_path
 		end
